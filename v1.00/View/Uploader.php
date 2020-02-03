79aa344684276c13ac8188686c286753c3dfc390
jeremyannereau <jeremyannereau@gmail.com>
2020-02-03 16:43:59 +0100

    uploader

---
+++ v1.00/View/Uploader.php
@@ -0,0 +1,8 @@
+<form action="../index.php?action=Uploader" method="POST">
+    <input type="text" name = "filename" placeholder="filename"/>
+    <input type="text" name = "status" value="public"/>
+    <input type="text" name = "link" placeholder="link"/>
+    <input type="text" name = "tags" placeholder="#tags"/>
+    <input type="text" name = "iduser" placeholder="#iduser"/>
+    <input type="submit" name = "submit" value = "S'inscrire">
+</form>
