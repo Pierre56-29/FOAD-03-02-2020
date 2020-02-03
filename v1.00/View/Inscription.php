ed0143ed7acdb05271ea5e79e45cf933790df796
jeremyannereau <jeremyannereau@gmail.com>
2020-02-03 15:44:46 +0100

    form inscription

---
+++ v1.00/View/Inscription.php
@@ -0,0 +1,7 @@
+
+<form action="index.php?action=Inscrire" method="POST">
+    <input type="text" name = "login" placeholder="login"/>
+    <input type="text" name = "email" placeholder="email"/>
+    <input type="password" name = "password" placeholder="password"/>
+    <input type="submit" name = "submit" value = "S'inscrire">
+</form>
