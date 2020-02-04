d4e4666022aa6ba57c5d6e696c52802736e40305
Pierre <pierre.bridonneau@laposte.net>
2020-02-04 11:40:39 +0100

    Class vue pour générer vues

---
+++ v1.00/View/View.php
@@ -0,0 +1,34 @@
+<?php
+
+class View
+{
+    private $fichier; // la vue à afficher
+
+    public function __construct($action)
+    {
+        $this->fichier = "View/". $action .".php";
+    }
+
+    public function generer($data)
+    {
+        $contenu = $this->genererFichier($this->fichier, $data);
+        $maPage =  array('content' => $contenu);
+        $vue = $this->genererFichier('View/Template.php', $maPage);
+        echo $vue;
+    }
+
+    private function genererFichier($fichier, $data)
+    {
+        if(file_exists($fichier))
+        {
+            extract($data);
+            ob_start();
+            require $fichier;
+            return ob_get_clean();
+        }
+        else
+        {
+            throw new Exception ("Fichier $fichier introuvable");
+        }
+    }
+}
