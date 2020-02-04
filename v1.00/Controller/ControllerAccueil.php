e76e13e34eedd3d650e79f2f77fb96b76756975c
Pierre <pierre.bridonneau@laposte.net>
2020-02-04 14:47:21 +0100

    modif bdd + requetes

--- v1.00/Controller/ControllerAccueil.php
+++ v1.00/Controller/ControllerAccueil.php
@@ -9,12 +9,25 @@ class ControllerAccueil
 
     public function renderAccueilAnonymous()
     {
+        $picturesAccueil = new PictureManager();
+        $pictures =$picturesAccueil->getPublicPictures(4);
+        var_dump($pictures);
+        die();
+        forEach($pictures as $picture)
+        {
+            $commentsImage = new CommentManager();
+            $commentsImage->getCommentsCountByArticle($picture->getIdPicture());
+            $picture['CommentsCount'] = $commentsImage;
+
+            $userName = new UserManager();
+            $userName->getUser();
+        }
         $vue = new View("AccueilNonInscrit");
         $vue->generer();
     }
 
     public function renderDashboard()
     {
-        
+
     }
 }
--- v1.00/Model/EntityManager/PictureManager.php
+++ v1.00/Model/EntityManager/PictureManager.php
@@ -5,7 +5,7 @@ class PictureManager extends Modele
     public function add(Picture $picture)
     {
         //$_SESSION['id_user'] = 1;
-        $req ='INSERT INTO picture(file_name, status, link, tags, id_user) VALUES(?, ?, ?, ?, ?)';
+        $req ='INSERT INTO picture(fileName, status, link, tags, idUser) VALUES(?, ?, ?, ?, ?)';
         if(empty($_SESSION['id_user']))
         {
             $this->executeReq($req,array($picture->getFilename(), $picture->getStatus(),$picture->getLink(), $picture->getTags(),1)); 
@@ -18,12 +18,12 @@ class PictureManager extends Modele
 
     public function getPicturesDashboard($id_user)
     {
-        $req='SELECT * FROM picture WHERE id_user=?';
+        $req='SELECT * FROM picture WHERE idUser=?';
         $res = $this->executeReq($req,$id_user);
         
         $pictures = [];
 
-        while($donnes =$res->fetch(PDO::ASSOC))
+        while($donnes =$res->fetch(PDO::FETCH_ASSOC))
         {
             $picture = new Picture();
             $picture->hydrate($donnes);
@@ -33,14 +33,21 @@ class PictureManager extends Modele
         return $pictures;
     }
 
-    public function getPublicPictures()
+    public function getPublicPictures($limit = 0)
     {
-        $req='SELECT * FROM picture WHERE status="public" ORDER BY date_upload DESC';
+        if($limit !== 0)
+        {
+            $req='SELECT * FROM picture WHERE status="public" ORDER BY dateUpload DESC LIMIT '. $limit;
+        }
+        else 
+        {
+            $req='SELECT * FROM picture WHERE status="public" ORDER BY dateUpload DESC';
+        }
         $res = $this->executeReq($req);
         
         $pictures = [];
 
-        while($donnes =$res->fetch(PDO::ASSOC))
+        while($donnes =$res->fetch(PDO::FETCH_ASSOC))
         {
             $picture = new Picture();
             $picture->hydrate($donnes);
@@ -50,9 +57,10 @@ class PictureManager extends Modele
         return $pictures;
     }
 
+
     public function delete($idPicture)
     {
-        $req='DELETE * from pictures WHERE id_picture = ?';
+        $req='DELETE * from pictures WHERE idPicture = ?';
         $res = $this->executeReq($req,$idPicture);
 
         return "Supprimé avec succès";
